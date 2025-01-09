87efdc979d491aa2bf4e923bcabb2207e9f503bd
ashish panchal <ashishpanchal199@gmail.com>
2025-01-10 03:17:44 +0530

    header

--- src/App.js
+++ src/App.js
@@ -17,25 +17,7 @@ import "./assets/scss/theme.scss";
 import fakeBackend from "./helpers/AuthType/fakeBackend";
 import AppRoute from "./routes/route";
 
-//Firebase helper
-// import { initFirebaseBackend } from "./helpers/firebase_helper";
-
-// Activating fake backend
 fakeBackend();
-
-// const firebaseConfig = {
-// 	apiKey: process.env.REACT_APP_APIKEY,
-// 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
-// 	databaseURL: process.env.REACT_APP_DATABASEURL,
-// 	projectId: process.env.REACT_APP_PROJECTID,
-// 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-// 	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-// 	appId: process.env.REACT_APP_APPID,
-// 	measurementId: process.env.REACT_APP_MEASUREMENTID,
-// };
-
-// init firebase backend
-// initFirebaseBackend(firebaseConfig);
 
 class App extends Component {
   constructor(props) {
@@ -66,30 +48,6 @@ class App extends Component {
 
     return (
       <React.Fragment>
-        {/* <Router>
-					<Routes>
-						{publicRoutes.map((route, idx) => (
-							<AppRoute
-								path={route.path}
-								layout={NonAuthLayout}
-								component={route.component}
-								key={idx}
-								isAuthProtected={false}
-							/>
-						))}
-
-						{authProtectedRoutes.map((route, idx) => (
-							<AppRoute
-								path={route.path}
-								layout={Layout}
-								component={route.component}
-								key={idx}
-								isAuthProtected={true}
-							/>
-						))}
-					</Routes>
-				</Router> */}
-
         <Routes>
           {publicRoutes.map((route, idx) => (
             <Route
