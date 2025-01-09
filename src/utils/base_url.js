105334e37d633bccc8f579c24dce5874783c59c2
ashish panchal <ashishpanchal199@gmail.com>
2025-01-09 13:58:38 +0530

    url

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,3 +1,3 @@
-// export const base_url = "https://mern-e-commerce-backend-1.onrender.com/api/";
+export const base_url = "https://mern-e-commerce-backend-1.onrender.com/api/";
 
-export const base_url = "http://localhost:5000/api/";
+// export const base_url = "http://localhost:5000/api/";
